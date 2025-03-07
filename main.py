d598f71ae55cffa341b2eb544522b073a4dd801e
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-03-07 10:48:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 #debug game and make sure every move works as intended, send to uncle Tim and David to test and give feedback, afterwards add finishing touches and last wave of testing, then release as finished version
 
 #import stuff
-import time, random, json
-
+import random, json
+from time import sleep
 
 with open('config.json') as config_file:
     config = json.load(config_file)
 
 #intro, rules, and how to play
 print("Hello and Welcome to Untitled Turn-Based Battle version Alpha")
-time.sleep(1)
+sleep(1)
 print("\nHow to play:\nFirst, before the battle starts, you select one attack for each category: Weapon, Spell, Shield, Potion, and Finisher. \nDuring the battle, you get to choose your attacks until you run out of stamina points. Then, your opponent does the same. This continues until one of the players lose all of their health, the one who remains is the winner!")
-time.sleep(3)
+sleep(3)
 print("\nRules: Both players have 1000 health, or HP and 2 stamina points, or SP. Each attack uses SP from 0.5 to 2. You regain all SP at the end of your turn. Finishers can only be used once per player when they have 500 HP or less, and it uses 2 SP.")
-time.sleep(3)
+sleep(3)
 print("\nEffects: \nBurn: lose 50 HP at the end of your turn, lasts 3 turns\nPoison: lose 30 HP at the end of your turn, lasts 5 turns\nBleed: lose 10 HP every turn + 10 HP for every hit taken, lasts 10 turns or until healed\nSummon: at the end of your turn, lose 30 HP\n")
-time.sleep(3)
+sleep(3)
 print("\nWhen the battle starts, type the attack you want to use and hit enter to use it if posible, enter 'end' to end your turn when you are out of SP")
-time.sleep(3)
+sleep(3)
 
 
 #set up variables
@@ -45,10 +45,11 @@ class Player:
         self.moveset = [weapon, spell, shield, potion, finisher]
 
 class Action:
-    def __init__(self, target, type, healthchange, sp, effectchance, effect = ""):
+    def __init__(self, target, type, heal, damage, sp, effectchance, effect = ""):
         self.target = target
         self.type = type #physical weapon attack, magic attack, healing, finisher, armor
-        self.healthchange:int = healthchange
+        self.heal = heal
+        self.damage = damage
         self.stamina_cost = sp
         self.effectchance = effectchance
         if effect: 
@@ -58,7 +59,7 @@ class Action:
 
 
 
-time.sleep(0.5)
+sleep(0.5)
 
 #set up lists
 weapons = config["weapons"]
@@ -66,22 +67,25 @@ spells = config["spells"]
 shields = config["shields"]
 potions = config["potions"]
 finishers = config["finishers"]
-time.sleep(0.5)
+sleep(0.5)
+
+#set up moves
+
 
 #player chooses moves
 #
 player = Player(input("Enter Username:\n"), 'weapon', 'spell', 'shield', 'potion', 'finisher')
 
 player.moveset[0] = str(input("Select a Weapon: \nSword: deals 50 HP with 50% chance to inflict bleed, 1 SP\nHammer: 50% to deal 100 HP, 50% chance to do nothing, 1 SP\nDagger: deals 20 HP, 25% chance to bleed, 0.5 SP\nBow and Arrow: hits 1 to 5 times, each hit deals 20 HP, 1 SP\nGuantlets: deals 70 HP, 1 SP\n"))
-time.sleep(0.5)
+sleep(0.5)
 player.moveset[1] = str(input("Select a Spell: \nIgnis: deals 30 HP, 50% chance to inflict burn, 1 SP\nGlacies: deals 30 HP, opponent loses 1/2 SP, 1 SP \nTempestas: hits 1 to 3 times, each hit deals 30 HP, 1 SP\nVenenum: deals 30 HP, 50% chance to inflict poison, 1 SP\nVocare: deals 30 HP, inflicts summon effect, 1 SP\n"))
-time.sleep(0.5)
+sleep(0.5)
 player.moveset[2] = str(input("Select a Shield: \nArmor: next weapon attack deals 50 less HP\nBarrier: next spell attack deals 50 less HP\n"))
-time.sleep(0.5)
+sleep(0.5)
 player.moveset[3] = str(input("Select a Potion: \nHeal: heal 100 HP\nRegen: for the next 3 turns, heal 50 HP\nCure: heal 50 HP and remove effects\n"))
-time.sleep(0.5)
+sleep(0.5)
 player.moveset[4] = str(input("Select a Finisher: \nLethal Execution: deals 500 HP, inflicts bleed, 2 SP\nMagus Exponentia Inspiratione: hits 1-3 times, each hit deals 200 HP, inflicts burn and poison, opponent loses 1/2 SP, 2 SP\nSteel-Fist Beatdown: hits 1-10 times, each hit deals 100 HP, 2 SP\n"))
-time.sleep(0.5)
+sleep(0.5)
 print(player.moveset)
 
 #bot chooses moves
@@ -116,6 +120,9 @@ while run and player.hp > 0 and cpu.hp > 0:
         attack = input("\nEnter attack:\n")
         if attack.upper() == player.moveset[0] or attack == player.moveset[1] or attack == player.moveset[2] or attack == player.moveset[3] or attack == player.moveset[4]:
             print(attack.upper())
+            player.hp += attack.upper.heal
+            computer.hp -= attack.upper.hurt
+
         myturn = False
     
     while not myturn:
@@ -135,7 +142,7 @@ if computer.hp <=0:
 
 #start battle
 while HP1 > 0 and HP2 > 0:
-    time.sleep(1)
+    sleep(1)
     print("Player's turn")
     armor1 = False #player has armor ready
     barrier1 = False #player has barrier ready
@@ -332,7 +339,7 @@ while HP1 > 0 and HP2 > 0:
             finisher1 = True
         else:
             print("Either you don't have that move equipped, you haven't fulfilled the conditions for using  finisher, or you miss-spelled it. Please try again. ")
-        time.sleep(1)
+        sleep(1)
         print("\nPlayer HP: "+str(HP1)+"\nCPU HP: "+str(HP2)+"\nPlayer SP: "+str(SP1))
         #check for effects
     if bleed1 == True:
@@ -368,7 +375,7 @@ while HP1 > 0 and HP2 > 0:
         if regenT1 == 0:
             regen1 = False
     SP1 = 2
-    time.sleep(1)
+    sleep(1)
     print("CPU's turn")
     armor2 = False #player has armor ready
     barrier2 = False #player has barrier ready
