e945545e8715a832179508a93a49839c16c04d4d
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-03-20 12:01:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -106,27 +106,27 @@ Tun into function so that incorrect inputs don't crash game, but instead just re
 """
 player.moveset[0] = str(input(f"Select a Weapon: \nSword(1): deals 50 HP with 50% chance to inflict bleed, 1 SP\nHammer(2): 50% to deal 100 HP, 50% chance to do nothing, 1 SP\nDagger(3): deals 20 HP, 25% chance to bleed, 0.5 SP\nBow and Arrow(4): hits 1 to 5 times, each hit deals 20 HP, 1 SP\nGuantlets(5): deals 70 HP, 1 SP\n"))
 #sleep(0.5)
-if player.moveset[0].is_integer() == False and player.moveset[0] != weapons[0] or player.moveset[1] != weapons[1] or player.moveset[2] != weapons[2] or player.moveset[3] != weapons[3] or player.moveset[4] != weapons[4]:
+if type(player.moveset[0]) == int and player.moveset[0] != weapons[0] or player.moveset[1] != weapons[1] or player.moveset[2] != weapons[2] or player.moveset[3] != weapons[3] or player.moveset[4] != weapons[4]:
     player.moveset[0] = weapons[int(player.moveset[0])-1]
 
 player.moveset[1] = str(input(f"Select a Spell: \nIgnis(1): deals 30 HP, 50% chance to inflict burn, 1 SP\nGlacies(2): deals 30 HP, opponent loses 1/2 SP, 1 SP \nTempestas(3): hits 1 to 3 times, each hit deals 30 HP, 1 SP\nVenenum(4): deals 30 HP, 50% chance to inflict poison, 1 SP\nVocare(5): deals 30 HP, inflicts summon effect, 1 SP\n"))
 #sleep(0.5)
-if player.moveset[1].is_integer() == False and player.moveset[1] != spells[0] or player.moveset[1] != spells[1] or player.moveset[2] != spells[2] or player.moveset[3] != spells[3] or player.moveset[4] != spells[4]:
+if type(player.moveset[1]) == int and player.moveset[1] != spells[0] or player.moveset[1] != spells[1] or player.moveset[2] != spells[2] or player.moveset[3] != spells[3] or player.moveset[4] != spells[4]:
     player.moveset[1] = spells[int(player.moveset[1])-1]
 
 player.moveset[2] = str(input(f"Select a Shield: \nArmor(1): next weapon attack deals 50 less HP\nBarrier(2): next spell attack deals 50 less HP\n"))
 #sleep(0.5)
-if player.moveset[2].is_integer() == False and player.moveset[2] != shields[0] or player.moveset[2] != shields[1]:
+if type(player.moveset[2]) == int and player.moveset[2] != shields[0] or player.moveset[2] != shields[1]:
     player.moveset[2] = shields[int(player.moveset[2])-1]
 
 player.moveset[3] = str(input(f"Select a Potion: \nHeal(1): heal 100 HP\nRegen(2): for the next 3 turns, heal 50 HP\nCure(3): heal 50 HP and remove effects\n"))
 #sleep(0.5)
-if player.moveset[3].is_integer() == False and player.moveset[3] != potions[0] or player.moveset[3] != potions[1] or player.moveset[3] != potions[2]:
+if type(player.moveset[3]) == int and player.moveset[3] != potions[0] or player.moveset[3] != potions[1] or player.moveset[3] != potions[2]:
     player.moveset[3] = potions[int(player.moveset[3])-1]
 
 player.moveset[4] = str(input(f"Select a Finisher: \nLethal Execution(1): deals 500 HP, inflicts bleed, 2 SP\nMagus Exponentia Inspiratione(2): hits 1-3 times, each hit deals 200 HP, inflicts burn and poison, opponent loses 1/2 SP, 2 SP\nSteel-Fist Beatdown(3): hits 1-10 times, each hit deals 100 HP, 2 SP\n"))
 #sleep(0.5)
-if player.moveset[4].is_integer() == False and player.moveset[4] != finishers[0] or player.moveset[4] != finishers[1] or player.moveset[4] != finishers[2]:
+if type(player.moveset[4]) == int and player.moveset[4] != finishers[0] or player.moveset[4] != finishers[1] or player.moveset[4] != finishers[2]:
     player.moveset[4] = finishers[int(player.moveset[4])-1]
 
 print(f"{player.moveset[0]}(1), {player.moveset[1]}(2), {player.moveset[2]}(3), {player.moveset[3]}(4), {player.moveset[4]}(5)")
@@ -199,7 +199,7 @@ while run and player.hp > 0 and cpu.hp > 0:
         #conditions to end turn: skip the rest of your turn or run out of stamina
         attack = input("\nEnter attack:\n").upper()
         print(attack)
-        if attack != player.moveset[0] or attack != player.moveset[1] or attack != player.moveset[2] or attack != player.moveset[3] or attack != player.moveset[4]:
+        if attack != player.moveset[0] or attack != player.moveset[1] or attack != player.moveset[2] or attack != player.moveset[3] or attack != player.moveset[4] and attack.upper() != "END":
             attack = player.moveset[int(attack)-1]
         if attack == player.moveset[0] or attack == player.moveset[1] or attack == player.moveset[2] or attack == player.moveset[3] or attack == player.moveset[4]:
             print(attack)
@@ -207,7 +207,7 @@ while run and player.hp > 0 and cpu.hp > 0:
         elif attack.upper() == 'END' or player.sp == 0:
             myturn = False
             print("End of Player's Turn")
-        for effect in player.effects
+        for effect in player.effects:
             Status(f"{player.name}", effect)
     
     while not myturn:
